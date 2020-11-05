5425f51cb893d132c155533253c9a5c27203f53d
Chua Hou <human+github@chuahou.dev>
2020-11-06 03:56:30 +0800

    build.sh: add script to build

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2020 Chua Hou
+
+# function to complain that upstream tarball does not exist
+print_gettar () {
+	echo "Ensure that the upstream tarball has been downloaded to"
+	echo "../$(basename $(dirname $(realpath $0)))_x.x.x.orig.tar.gz"
+}
+
+# ensure we are in script directory
+if [ $(dirname $0) != '.' ]; then
+	echo "Invoke this script from its directory!"
+	exit 1
+fi
+
+# check for upstream tarball
+UPTAR=$(ls ../$(basename $(dirname $(realpath $0)))_*.orig.tar.gz || \
+	print_gettar)
+
+# extract tarball and move its contents, sans hidden files, out
+tar xzf "$UPTAR"
+cp -r Iosevka-*/* .
+rm Iosevka-*/ -rf
+
+# build packages
+debuild -us -uc -b
+debuild -T clean
